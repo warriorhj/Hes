105368ebf58b36ebc8cf1202cca3894613cd90e6
meihao <warrior607@163.com>
2023-07-17 17:56:45 +0800

    feat:1

--- src/pages/api/meter/deleteMeter/[meterno].js
+++ src/pages/api/meter/deleteMeter/[meterno].js
@@ -4,7 +4,7 @@
  * @Author: Hao
  * @Date: 2023-07-17 13:22:38
  * @LastEditors: Hao
- * @LastEditTime: 2023-07-17 13:27:08
+ * @LastEditTime: 2023-07-17 17:49:30
  * @FilePath: \hes\src\pages\api\meter\deleteMeter\[meterno].js
  */
 import connectDB from '../../../../../middleware/mongodb'
@@ -17,8 +17,13 @@ const handler = async (req, res) => {
     // const result = await Meter.deleteOne(req.query)
     // const test = [{meterno:"warrior2"},{meterno:"warriortest"}]
     // const result = await Meter.deleteMany({meterno:{$in: ['warrior2', 'warriortest']}})
-    console.log(typeof req.query,Object.values(req.query))
+    // console.log(typeof req.query,typeof Object.values(req.query)[0]);
+    const query =  Object.values(req.query)[0];
+    // console.log(query.split(','));
     const deleteList = Object.values(req.query);
+    console.log(typeof query,typeof deleteList, query, deleteList)
+    const test = JSON.parse(query);
+    console.log(test);
     const result = await Meter.deleteMany({meterno:{$in: deleteList}})
     console.log(result)
     return res.status(200).send(result)
